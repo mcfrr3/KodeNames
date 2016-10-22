6727f45465d196181b234bab5f6dcc29e9a74d93
sw-tracker <oldiazg@gmail.com>
2016-10-22 15:10:11 +0200

    Fix array overflow bug. Validate select on change to avoid multiple popups.

--- scripts/index.js
+++ scripts/index.js
@@ -29,6 +29,27 @@ $("#seed").keyup(function() {
 });
 
 $("#gameMode").change(function() {
+	var option = $('#gameMode :selected').val();
+	switch (option) {
+		case 'local_pic':
+			if (window.File && window.FileReader && window.FileList && window.Blob) {
+				// Great success! All the File APIs are supported.
+			} else {
+				alert('The File APIs are not fully supported in this browser.');
+				document.getElementById('gameMode').value = "default";
+			}
+			if (local_images.length < NUMBER_OF_PICTURES) {
+				alert('Click on the BROWSE button and select some pictures first. Minimum ' + NUMBER_OF_PICTURES + ' images.');
+			}
+			break;
+		case 'cah':
+			if (window.confirm("This uses X-Rated content. Are you older than 18?")) {
+				// we are old enough
+			} else {
+				document.getElementById('gameMode').value = "default";
+			}
+			break;
+	}
 	fire();
 });
 
@@ -73,29 +94,6 @@ function fire() {
 
 	var option = $('#gameMode :selected').val();
 	number_of_items = NUMBER_OF_WORDS;
-
-	switch (option) {
-		case 'local_pic':
-			if (window.File && window.FileReader && window.FileList && window.Blob) {
-				// Great success! All the File APIs are supported.
-			} else {
-				alert('The File APIs are not fully supported in this browser.');
-				document.getElementById('gameMode').value = "default";
-				option = "default";
-			}
-			if (local_images.length < NUMBER_OF_PICTURES) {
-				alert('Click on the BROWSE button and select some pictures first. Minimum ' + NUMBER_OF_PICTURES + ' images.');
-			}
-			break;
-		case 'cah':
-			if (window.confirm("This uses X-Rated content. Are you older than 18?")) {
-				// we are old enough
-			} else {
-				document.getElementById('gameMode').value = "default";
-                option = "default";
-			}
-			break;
-	}
 
 	if (option == 'local_pic') {
 		document.getElementById("files").style.visibility = "visible";
@@ -198,8 +196,11 @@ function createNewGame() {
 		if (!trs[i % items_per_row]) {
 			trs[i % items_per_row] = "";
 		}
-		var randomNumber = Math.floor(Math.random() * sessionData.length);
+		var randomNumber = Math.floor(Math.random() * (sessionData.length-1));
 		var word = sessionData[randomNumber];
+		if (typeof word == "undefined") {
+			word = "";
+		}
 		var grayscale_pic = "";
 		removeItem(sessionData, randomNumber);
 		wordsSelected.push(word);
@@ -214,7 +215,7 @@ function createNewGame() {
 				trs[i % items_per_row] += "<div class=\"word\" id=\'" + i + "\' onclick=\"clicked(\'" + i + "\')\"><div><a href=\"#\"><img id=\'pic" + i + "\' src=\"https://unsplash.it/" + grayscale_pic + imgWidth + "/" + imgHeight + "?image=" + word + "\"><span class=\"ada\"></span></a></div></div>";
 				break;
 			case 'local_pic':
-				trs[i % items_per_row] += "<div class=\"word\" id=\'" + i + "\' onclick=\"clicked(\'" + i + "\')\"><div><a href=\"#\"><img id=\'pic" + i + "\' " + word + "<span class=\"ada\"></span></a></div></div>";
+				trs[i % items_per_row] += "<div class=\"word\" id=\'" + i + "\' onclick=\"clicked(\'" + i + "\')\"><div><a href=\"#\">" + word + "<span class=\"ada\"></span></a></div></div>";
 				break;
 			default:
 				trs[i % items_per_row] += "<div class=\"word\" id=\'" + i + "\' onclick=\"clicked(\'" + i + "\')\"><div><a href=\"#\"><span class=\"ada\"></span>" + word + "</a></div></div>";
@@ -272,6 +273,7 @@ function doTint(value, colour) {
 	if (colour == "black" || colour == COLOR_BLACK) {
 		document.getElementById(value).style.color = "white";
 		document.body.style.backgroundColor = COLOR_GRAY;
+		alert("You lost!");
 	}
 }
 
@@ -350,10 +352,10 @@ function spyMaster() {
 	//TODO: randomize or organize tiles for easier comparing
 	spyMasterMode = true;
 	for (var i = 0; i < number_of_items; i++) {
-    console.log(teams[i]);
+    	//console.log(teams[i]);
 		document.getElementById(i).style.backgroundColor = teams[i];
 		if (teams[i] == "black" || teams[i] === COLOR_BLACK) {
-      console.log(i, document.getElementById(i));
+      		//console.log(i, document.getElementById(i));
 			document.getElementById(i).style.color = "white";
 		}
 	}
